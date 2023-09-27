7fee2566c1e5f65b41913146bfae4a2dc1b97ea3
provodd <provod.dev@gmail.com>
2023-09-27 14:23:13 +0300

    Подправил основные конфигурационные файлы

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,15 @@
 import './bootstrap';
-import {createApp} from 'vue';
+import '../css/app.css';
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import vuex from './store/store.js';
-import router from "./router/router.js";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import { createApp } from 'vue';
+import router from "./router/router";
+import store from "./store/store.js";
+
 
 const app = createApp({
 
 });
-app.use(VueAxios, axios);
-app.use(vuex);
-app.use(router);
-app.provide('axios', app.config.globalProperties.axios)
-app.mount('#app');
+
+app.use(router).use(store).mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,35 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: '/',
     plugins: [
-        vue({
-            template:{
-                transformAssetUrls:{
-                    base: '/',
-                    includeAbsolute: false,
-                }
-            },
-        }),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/bootstrap.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                }
+            }
+        }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true
+        }
+    },
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
-            // '@': path.resolve(__dirname, './src/assets')
         },
     },
 });
